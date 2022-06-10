5a3393090d565783200915b0373a436109124c74
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2022-06-11 01:15:43 +0200

    exhaustMap Operator

--- dist/48b4e2471481a507263c6b942d768823.js
+++ dist/48b4e2471481a507263c6b942d768823.js
@@ -11698,7 +11698,7 @@ var _operators = require("rxjs/operators");
 var _ajax = require("rxjs/ajax");
 
 const button = document.querySelector("#btn");
-const observable = (0, _rxjs.fromEvent)(button, "click").pipe((0, _operators.concatMap)(() => {
+const observable = (0, _rxjs.fromEvent)(button, "click").pipe((0, _operators.exhaustMap)(() => {
   return (0, _rxjs.interval)(1000).pipe((0, _operators.take)(5), (0, _operators.tap)({
     complete() {
       console.log("complete");
